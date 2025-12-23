54ba42a8c85ecf23bcbf3697d7d17c4309e29746
Ishant Sharma <ishantsharma9499@gmail.com>
2025-12-23 18:50:28 +0530

    Time: 13 ms (79.94%) | Memory: 51.9 MB (68.12%) - LeetSync

---
+++ 816-design-hashset/design-hashset.java
@@ -0,0 +1,27 @@
+class MyHashSet {
+
+    boolean [] setArray = new boolean[1000000+1];
+    public MyHashSet() {
+        
+    }
+    
+    public void add(int key) {
+        setArray[key] = true;
+    }
+    
+    public void remove(int key) {
+        setArray[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return setArray[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
